a138a1c3bd0372a9d5776379230ac4b399313b2d
niico <5653050+niicojs@users.noreply.github.com>
2023-06-04 17:11:48 +0200

    Create install.cmd

---
+++ install.cmd
@@ -0,0 +1,13 @@
+# general tools
+winget install Google.Chrome.Beta
+winget install Microsoft.Powertoys 
+
+# node dev
+winget install pnpm
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.PowerShell
+winget install Git.Git
+pnpm env use --global lts
+
+# data
+winget install Microsoft.AzureDataStudio.Insiders
