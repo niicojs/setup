1f487662bb71c53cf03b1ba633376e15b6343fc1
niico <5653050+niicojs@users.noreply.github.com>
2026-01-17 16:10:45 +0100

    up

--- 01-init.ps1
+++ 01-init.ps1
@@ -3,6 +3,9 @@ irm bun.sh/install.ps1 | iex
 
 # pnpm and node
 Invoke-WebRequest https://get.pnpm.io/install.ps1 -UseBasicParsing | Invoke-Expression
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
 pnpm env use --global lts
 
 # system settings
@@ -14,13 +17,15 @@ Set-ItemProperty $key ShowSuperHidden 1
 # apps
 winget install --id=Microsoft.PowerShell
 winget install --id=Google.Chrome.Beta
-winget install --id=Microsoft.VisualStudioCode
+winget install vscode
 winget install --id=Starship.Starship
 winget install --id=dotPDN.PaintDotNet
 winget install --id=Telegram.TelegramDesktop
 winget install --id=SST.opencode
 winget install --id=Git.Git
 winget install --id=GitHub.GitHubDesktop
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 # script
 bun 02-install.js
