41616283e9f241e34cad591cc2873e5d4b1b42ae
niico <5653050+niicojs@users.noreply.github.com>
2026-05-01 15:59:48 +0200

    up

--- 01-init.ps1
+++ 01-init.ps1
@@ -1,12 +1,11 @@
 # bun
 irm bun.sh/install.ps1 | iex
 
-# pnpm and node
-Invoke-WebRequest https://get.pnpm.io/install.ps1 -UseBasicParsing | Invoke-Expression
+irm https://vite.plus/ps1 | iex
 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
-pnpm env use --global lts
+vp env default lts
 
 # system settings
 $key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
@@ -15,15 +14,18 @@ Set-ItemProperty $key HideFileExt 0
 Set-ItemProperty $key ShowSuperHidden 1
 
 # apps
+winget install --id=7zip.7zip
 winget install --id=Microsoft.PowerShell
 winget install --id=Google.Chrome.Beta
-winget install vscode
+winget install --id=Microsoft.VisualStudioCode.Insiders
 winget install --id=Starship.Starship
 winget install --id=dotPDN.PaintDotNet
 winget install --id=Telegram.TelegramDesktop
-winget install --id=SST.opencode
+winget install --id=SST.OpenCodeDesktop
 winget install --id=Git.Git
-winget install --id=GitHub.GitHubDesktop
+winget install --id=GitHub.GitHubDesktop 
+winget install --id=Tailscale.Tailscale
+winget install --id=Microsoft.PowerToys
 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
